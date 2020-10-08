69f043b0e64f3872a3002eb119d377792b603b8a
Mina <mina.park127@gmail.com>
2020-10-08 15:50:47 +1100

    change the entry file

--- app.js
+++ app.js
@@ -7,14 +7,9 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
-
 const handleHome = (req, res) => res.send("hello from home");
 
-app.use(helmet);
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,4 +17,4 @@ app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
-app.listen(PORT, handleListening);
+export default app;
