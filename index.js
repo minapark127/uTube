c1c0107772c4cca10b4cc18a61d4264f18a472c1
Mina <mina.park127@gmail.com>
2020-10-08 15:40:45 +1100

    installing middlewares

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import "core-js";
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -8,6 +13,12 @@ const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("hello from home");
+
+app.use(helmet);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
