679a419d7e766ddc9ec3b601da351f57efebf892
Mina <mina.park127@gmail.com>
2020-10-14 15:23:35 +1100

    add HTML forms

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,9 @@
 import routes from "./routes";
+import { fontAwesomeKey } from "./key";
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "uTube";
   res.locals.routes = routes;
+  res.locals.fontAwesomeKey = fontAwesomeKey;
   next();
 };
