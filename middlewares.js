1eab92debe19db2b6d14303e823603e7e7449497
Mina <mina.park127@gmail.com>
2020-10-23 15:06:49 +1100

    fix up header

--- middlewares.js
+++ middlewares.js
@@ -7,9 +7,9 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "uTube";
   res.locals.routes = routes;
   res.locals.env = process.env;
-  //test user
+  // test user
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 1234,
   };
   next();
