d0c8c6cdc90587d30b08cf1b4266c8b5b3cdfbb2
Mina <mina.park127@gmail.com>
2020-11-04 19:33:30 +1100

    add current time/total time functionality

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,8 +1,11 @@
 const videoPlayer = document.querySelector("#js-videoPlayer");
-const video = document.querySelector("#js-video");
+const video = document.querySelector("#js-videoPlayer video");
 const playBtn = document.querySelector("#js-playBtn");
 const volumeBtn = document.querySelector("#js-volumeBtn");
 const fullScreenBtn = document.querySelector("#js-fullScreenBtn");
+const timebar = document.querySelector("#js-timeBar");
+const currentTimebar = document.querySelector("#js-timeBar-current");
+const totalTimebar = document.querySelector("#js-timeBar-total");
 
 const playClickHandler = () => {
   if (video.paused) {
@@ -34,10 +37,38 @@ const fullScreenClickHandler = () => {
   }
 };
 
+const formatTime = (seconds) => {
+  const secondsNumber = parseInt(seconds, 10);
+  const hours = Math.floor(secondsNumber / 3600)
+    .toString()
+    .padStart(2, "0");
+  const minutes = Math.floor((secondsNumber - hours * 3600) / 60)
+    .toString()
+    .padStart(2, "0");
+  const leftSeconds = (secondsNumber - hours * 3600 - minutes * 60)
+    .toString()
+    .padStart(2, "0");
+
+  if (hours === "00") {
+    return `${minutes}:${leftSeconds}`;
+  }
+  return `${hours}:${minutes}:${leftSeconds}`;
+};
+
+const setVideoTime = () => {
+  totalTimebar.innerHTML = formatTime(video.duration);
+};
+
+const setCurrentTime = () => {
+  currentTimebar.innerHTML = formatTime(video.currentTime);
+};
+
 const init = () => {
   playBtn.addEventListener("click", playClickHandler);
   volumeBtn.addEventListener("click", volumeClickHandler);
   fullScreenBtn.addEventListener("click", fullScreenClickHandler);
+  video.addEventListener("loadedmetadata", setVideoTime);
+  video.addEventListener("timeupdate", setCurrentTime);
 };
 
 if (videoPlayer) {
