e0963c09509ee57ddd459f98cefb42e93a556a3c
Mina <mina.park127@gmail.com>
2020-11-06 20:38:53 +1100

    record and save video from upload

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -3,6 +3,36 @@ const startBtn = document.querySelector("#js-recordStart");
 const stopBtn = document.querySelector("#js-recordStop");
 const preview = document.querySelector("#js-videoRecorder video");
 
+let streamObj;
+let videoRecorder;
+
+const handleVideoData = (event) => {
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded_uTube.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  stopBtn.classList.toggle("recordBtn-showing");
+  startBtn.classList.toggle("recordBtn-showing");
+  preview.srcObject = null;
+  streamObj.getTracks().forEach((track) => {
+    track.stop();
+  });
+  stopBtn.removeEventListener("click", stopRecording);
+  startBtn.addEventListener("click", getVideo);
+};
+
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(streamObj);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  stopBtn.addEventListener("click", stopRecording);
+};
+
 const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
@@ -14,6 +44,8 @@ const getVideo = async () => {
     preview.play();
     startBtn.classList.toggle("recordBtn-showing");
     stopBtn.classList.toggle("recordBtn-showing");
+    streamObj = stream;
+    startRecording();
   } catch (error) {
     startBtn.innerHTML = "record unauthorised";
   } finally {
